3cce0d487a88f95990da8007d738734618a21ba6
Lakshmi Prabha M M <prabha4678@gmail.com>
2026-07-24 22:57:05 +0530

    Add files via upload

--- ex2code4.c
+++ ex2code4.c
@@ -1,56 +1,36 @@
 //LAKSHMI PRABHA M M
 // 25BAD049
-#include <stdio.h>
+include <stdio.h>
 
 int main() {
     int N;
-
-    printf("Enter the number of sectors: ");
-    scanf("%d", &N);
+    long long P;
+    scanf("%d %lld", &N, &P);
 
     int arr[N];
-
-    printf("Enter %d binary values (0 or 1): ", N);
     for (int i = 0; i < N; i++) {
         scanf("%d", &arr[i]);
     }
 
-    // Count total number of 1s
-    int countOnes = 0;
-    for (int i = 0; i < N; i++) {
-        if (arr[i] == 1)
-            countOnes++;
+    int start = 0, maxLen = 0;
+    long long currentSum = 0;
+
+    for (int end = 0; end < N; end++) {
+        currentSum += arr[end];
+
+        while (currentSum > P && start <= end) {
+            currentSum -= arr[start];
+            start++;
+        }
+
+        if (currentSum <= P) {
+            int len = end - start + 1;
+            if (len > maxLen)
+                maxLen = len;
+        }
     }
 
-    // If 0 or 1 active sectors, no swaps needed
-    if (countOnes <= 1) {
-        printf("Min Swaps: 0");
-        return 0;
-    }
-
-    // Count 1s in the first window
-    int currentOnes = 0;
-    for (int i = 0; i < countOnes; i++) {
-        if (arr[i] == 1)
-            currentOnes++;
-    }
-
-    int maxOnes = currentOnes;
-
-    // Slide the window
-    for (int i = countOnes; i < N; i++) {
-        if (arr[i] == 1)
-            currentOnes++;
-        if (arr[i - countOnes] == 1)
-            currentOnes--;
-
-        if (currentOnes > maxOnes)
-            maxOnes = currentOnes;
-    }
-
-    int minSwaps = countOnes - maxOnes;
-
-    printf("Min Swaps: %d", minSwaps);
+    printf("Max Length: %d\n", maxLen);
 
     return 0;
 }
